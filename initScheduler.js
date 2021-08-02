2ff9332f23222aead02e1db13076aaa27b43313d
Daniel Chistyakov <4i.danila@gmail.com>
2021-08-02 18:34:54 +0300

    Bugs fixed

--- initScheduler.js
+++ initScheduler.js
@@ -7,6 +7,6 @@ exports.initScheduler = async (bot) => {
     const data = await Preferences.find();
     data.map(async (item, key) => {
         const dbdata = await Preferences.findOne({ chatId: item.chatId }).exec();
-        Scheduler(bot, item.chatId, dbdata, item.interval, false);
+        Scheduler(bot, item.chatId, dbdata, item.interval, false, false);
     })
 }
